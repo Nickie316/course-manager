a04ab69877b70a5e2bd228e39044eae7d528f103
Nick <kankuro.mat@outlook.com>
2021-07-09 00:55:15 -0300

    Curso e Projeto Finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,27 @@
-import { Error404Component } from './error-404/error-404.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { ReplacePipe } from './pipe/replace.pipe';
-import { StarComponent } from './../star/star.component';
-import { CourseListComponent } from './../courses/course-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CourseInfoComponent } from 'src/courses/course-info.component';
+import { CourseModule } from 'src/courses/course.module';
+import { CoreModule } from './core/components/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
-    NavBarComponent,
-    Error404Component,
-    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
+    CourseModule,
+    CoreModule,
     RouterModule.forRoot([
-      {
-        path: 'courses', component: CourseListComponent
-      },
       
       {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
-
-      {
         path: '', redirectTo: 'courses', pathMatch: 'full' // Redireciona para a rota 
-      },
-
-      {
-        path: '**', component: Error404Component // Quando não encontra a Rota
       }
+      
     ])
   ],
   providers: [],
